ce941ca52449f727b89661bfd18b83abf901f3e7
Your Name <you@example.com>
2021-03-24 21:16:28 +0600

    hange app.js file

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import "./App.css"
+import AquariumBuilder from "./components/AquariumBuilder/AquariumBuilder";
 import Layout from "./components/Layout/Layout";
-
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        Awesome builder by Aiylcheva Tamara
+        <AquariumBuilder />
       </Layout>
     </div>
   );
