ab53005b52aab6339ff73b1c5cb5f5e37c5c25d4
Your Name <you@example.com>
2021-03-24 22:42:09 +0600

    aquarium previe stayl change

---
+++ src/components/AquariumBuilder/AquariumPreview/AquariumPreview.js
@@ -0,0 +1,24 @@
+import AquariumFish from "../AquariumFish/AquariumFish";
+import classes from "./AquariumPreview.module.css";
+import fishBackground from "../../../images/ocean.jpg";
+
+const AquariumPreview = ({ ingredients }) => {
+  const result = [];
+  for (const ingredient in ingredients) {
+    for (let i = 0; i < ingredients[ingredient]; i++) {
+      result.push(<AquariumFish type={ingredient} />)
+    }
+  }
+
+  return (
+    <div className={classes.AquariumPreview}>
+      <div
+        className={classes.ingredients}
+        style={{ backgroundImage: `url(${fishBackground})` }}>
+        {result}
+      </div>
+    </div>
+  );
+}
+
+export default AquariumPreview;
