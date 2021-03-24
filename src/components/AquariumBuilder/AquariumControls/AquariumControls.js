5b90ec85e338370fea4924820f56e8f3abe8f1d6
Your Name <you@example.com>
2021-03-24 21:53:49 +0600

    added aquarium control

---
+++ src/components/AquariumBuilder/AquariumControls/AquariumControls.js
@@ -0,0 +1,9 @@
+import classes from "./AquariumControls.module.css";
+
+const AquariumControls = () => {
+  return (
+    <div className={classes.AquariumControls}>Controls</div>
+  );
+}
+
+export default AquariumControls;
