5a50709f824163e37faeabdb63dbc5a7edac87f1
Your Name <you@example.com>
2021-04-07 23:05:59 +0600

    Added new changes to preview

--- src/components/AquariumBuilder/AquariumPreview/AquariumPreview.js
+++ src/components/AquariumBuilder/AquariumPreview/AquariumPreview.js
@@ -1,22 +1,26 @@
 import AquariumFish from "../AquariumFish/AquariumFish";
+
 import classes from "./AquariumPreview.module.css";
 import fishBackground from "../../../images/aquariumocean.jpg";
 
-const AquariumPreview = ({ ingredients }) => {
+const AquariumPreview = ({ ingredients, price }) => {
   const result = [];
   for (const ingredient in ingredients) {
     for (let i = 0; i < ingredients[ingredient]; i++) {
-      result.push(<AquariumFish type={ingredient} />)
+      result.push(<AquariumFish key={ingredient + i} type={ingredient} />)
     }
   }
 
   return (
     <div className={classes.AquariumPreview}>
-      <div
-        className={classes.ingredients}
-        style={{ backgroundImage: `url(${fishBackground})` }}>
-        {result}
+      <div className={classes.aquarium}>
+        <div
+          className={classes.ingredients}
+          style={{ backgroundImage: `url(${fishBackground})` }}>
+          {result}
+        </div>
       </div>
+      <div className={classes.price}>{price.toFixed(1)} som</div>
     </div>
   );
 }
