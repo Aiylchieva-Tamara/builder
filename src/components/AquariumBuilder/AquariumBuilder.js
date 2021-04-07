5a44b85e89400ff251f8ff042aff3a5859edc2be
Your Name <you@example.com>
2021-04-07 23:14:40 +0600

    fixed bugs in aquarium builder

--- src/components/AquariumBuilder/AquariumBuilder.js
+++ src/components/AquariumBuilder/AquariumBuilder.js
@@ -1,39 +1,64 @@
 import classes from "./AquariumBuilder.module.css";
 import AquariumPreview from "./AquariumPreview/AquariumPreview";
 import AquariumControls from "./AquariumControls/AquariumControls";
-import { useState } from "react";
+import { useEffect, useState } from "react";
+import axios from "axios";
+import Modal from "../UI/Modal/Modal";
 
 const AquariumBuilder = () => {
-  const [ingredients, setIngredients] = useState({
-    coliasis: 1,
-    labeo:1,
-    blue: 9,
-    angel: 1,
-    yelow: 1,
+  const prices = {
+    coliasis: 3.5,
+    labeo: 4,
+    blue: 0.3,
+    angel: 0.3,
+    yelow: 2,
     clown: 1,
-  });
+  };
+  const [ingredients, setIngredients] = useState({});
+  const [price, setPrice] = useState(0);
+
+  useEffect(
+    () =>
+      axios
+        .get("https://builder-8d5fc-default-rtdb.firebaseio.com/default.json")
+        .then((response) => {
+          setPrice(response.data.price);
+
+          // For arrays
+          // setIngredients(Object.values(response.data.ingredients));
+          // For objects
+          setIngredients(response.data.fish);
+        }),
+    []
+  );
 
   function addIngredient(type) {
-    const newIngredient = { ...ingredients};
+    const newIngredient = { ...ingredients };
     newIngredient[type]++;
+    setPrice(price + prices[type]);
     setIngredients(newIngredient);
   }
 
   function removeIngredient(type) {
-    const newIngredient = { ...ingredients};
-    newIngredient[type]--;
-    setIngredients(newIngredient);
+    if (ingredients[type]) {
+      const newIngredient = { ...ingredients };
+      newIngredient[type]--;
+      setPrice(price - prices[type]);
+      setIngredients(newIngredient);
+    }
   }
 
   return (
     <div className={classes.AquariumBuilder}>
-      <AquariumPreview ingredients={ingredients} />
-      <AquariumControls ingredients={ingredients}
-       addIngredient={addIngredient}
-       removeIngredient={removeIngredient}
-       />
+      <AquariumPreview ingredients={ingredients} price={price} />
+      <AquariumControls
+        ingredients={ingredients}
+        addIngredient={addIngredient}
+        removeIngredient={removeIngredient}
+      />
+      <Modal>Hello</Modal>
     </div>
   );
-}
+};
 
 export default AquariumBuilder;
