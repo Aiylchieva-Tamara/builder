7e0ff30da46d25854fae248b6e993fad8244e166
Your Name <you@example.com>
2021-04-16 14:42:21 +0600

    Add and use CHeckout component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,19 @@
 import Layout from "./components/Layout/Layout";
 import AquariumBuilder from "./components/AquariumBuilder/AquariumBuilder";
 import "./App.css";
+import Checkout from "./components/Checkout/Checkout";
+import { Redirect, Route, Switch } from "react-router";
 
 function App() {
   return (
     <div className="App">
       <Layout>
-        <AquariumBuilder />
+       
+        <Switch>
+          <Route path="/" component={AquariumBuilder} exact />
+          <Route path="/checkout" component={Checkout} />
+          <Redirect to="/" />
+        </Switch>
       </Layout>
     </div>
   );
