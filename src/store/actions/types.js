1ce7a9897267c08951256da46e555d707e938a60
Your Name <you@example.com>
2021-05-21 14:25:18 +0600

    Add auth action types

--- src/store/actions/types.js
+++ src/store/actions/types.js
@@ -3,3 +3,8 @@ export const REMOVE_FISH = "REMOVE_FISH";
 export const SET_FISH = "SET_FISH";
 
 export const SET_ORDERS = "SET_ORDERS";
+
+export const AUTH_START = "AUTH_START";
+export const AUTH_SUCCESS = "AUTH_SUCCESS";
+export const AUTH_FAIL = "AUTH_FAIL";
+export const AUTH_LOGOUT = "AUTH_LOGOUT"; 
