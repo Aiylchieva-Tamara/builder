ba647cc04e8d859a7cef18f2ff0d25935bd07811
Your Name <you@example.com>
2021-05-21 15:09:46 +0600

    Use submit button instead of useState

--- src/components/Auth/Auth.js
+++ src/components/Auth/Auth.js
@@ -9,14 +9,17 @@ import { Redirect, useLocation } from "react-router-dom";
 
 export default withAxios(() => {
   const dispatch = useDispatch();
-  const [ method, setMethod ] = useState(null);
+  
   const { loading, error, token } = useSelector(state => state.auth);
   const location = useLocation();
 
   const formSubmitted = (event) => {
+    
     start(dispatch);
 
     const data = new FormData(event.target);
+    const method = event.nativeEvent.submitter.innerText == "Sign in"
+    ? "signin" : "signup";
     auth(dispatch, method, data.get('email'), data.get('password'));
 
     event.preventDefault();
@@ -29,8 +32,8 @@ export default withAxios(() => {
         <h1>Welcome</h1>
         <input type="email" placeholder="E-mail" name="email" required />
         <input type="password" placeholder="Password" name="password" required minLength="6" />
-        <Button click={() => setMethod('signin')} green>Sign in</Button>
-        <Button click={() => setMethod('signup')} red>Sign up</Button>
+        <Button>Sign in</Button>
+        <Button>Sign up</Button>
       </form>
     );
   }
